3fb8eeb94ba69cd776e96984bdd21ae5f274dc91
miguelpg <migsub77@gmail.com>
2019-05-01 16:31:29 -0700

    adding project cards

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import ProjectCard from "../_global/ProjectCard";
+import _projects from '../../constants/data/projects.json';
 
 
 const Projects = ({lang}) => {
+  const renderProjects = (
+    _projects.map(item => {
+    
+    })
+  );
   return (
     <main className="projects">
       <section>
       <h1>Projects page</h1>
-        <ProjectCard lang={lang}/>
       </section>
     </main>
   );
