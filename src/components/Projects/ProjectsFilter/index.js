23f5b7d501fb13030bd86c64cfc3455fdd303e5d
miguelpg <miguelpg@ncsoft.com>
2019-05-22 14:36:46 -0700

    added filter stuff

--- src/components/Projects/ProjectsFilter/index.js
+++ src/components/Projects/ProjectsFilter/index.js
@@ -5,17 +5,15 @@ import {projects_filter} from '../../../constants/data/global';
 import classnames from 'classnames';
 
 
-const ProjectsFilter = ({currentFilter, changeFilterCB}) => {
-  
-  const renderNavItems = () => {
-    const is_active = false;
+function ProjectsFilter ({currentFilter, changeFilterCB}) {
+  this.renderNavItems () {
     return (<div className="nav-filter-items">
       {
         Object.keys(projects_filter).map((item, i) => {
           return (<div
             key={i}
             onClick={changeFilterCB.bind(this, item)}
-            className={classnames({['active']: is_active})}
+            className={classnames('nav-item' , {'active': currentFilter === item})}
           >
             {projects_filter[item]}
           </div>)
@@ -28,55 +26,10 @@ const ProjectsFilter = ({currentFilter, changeFilterCB}) => {
       <main className="projects-filter">
         <section>
           <div>{currentFilter}</div>
-          {renderNavItems()}
+          {this.renderNavItems()}
         </section>
       </main>
     )
 };
-
-// class ProjectsFilter extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       current_filter: '2019',
-//       is_active: false
-//     }
-//   }
-//
-//   handleFilterChange(current_filter) {
-//     this.setState({
-//       current_filter: current_filter
-//     })
-//   };
-//
-//   renderNavItems = () => {
-//     const is_active = this.state.is_active;
-//     return (<div className="nav-filter-items">
-//       {
-//         Object.keys(projects_filter).map((item, i) => {
-//           return (<div
-//             key={i}
-//             onClick={this.handleFilterChange.bind(this, item)}
-//             className={classnames({['active']: is_active})}
-//           >
-//             {projects_filter[item]}
-//           </div>)
-//         })
-//       }
-//     </div>);
-//   };
-//
-//   render() {
-//     const {current_filter} = this.state;
-//     return (
-//       <main className="projects-filter">
-//         <section>
-//           <div>{current_filter}</div>
-//           {this.renderNavItems()}
-//         </section>
-//       </main>
-//     )
-//   }
-// }
 
 export default withRouter(ProjectsFilter);
